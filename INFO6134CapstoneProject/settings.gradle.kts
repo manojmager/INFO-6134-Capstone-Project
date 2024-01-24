6bb84d2b82908716af2bec05f9507fdfff2d1b31
Manoj Rontija <manojrontija@Manojs-MBP.ht.home>
2024-01-23 22:29:00 -0500

    Init Project

---
+++ INFO6134CapstoneProject/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "INFO-6134 Capstone Project"
+include(":app")
